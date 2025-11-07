fafe6c162b58a0f35646a77bc786eb5ea63cc4b2
phadeline <phadelinee@gmail.com>
2025-11-06 21:30:03 -0500

    added alert

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const button = document.getElementById("connect");
 
-button.addEventListener("click")
+button.addEventListener("click", doSomething)
+
+function doSomething(){
+    alert("hello")
+}
