9040e0a76887608c5b14d7a5fe30d14a00627b3b
phadeline <phadelinee@gmail.com>
2025-11-15 23:49:00 -0500

    jquery

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-var button = document.getElementById("myRange");
+var button;
+
+$(document).ready(function(){button = document.getElementById("myRange")});
+
+const utcNowMilliseconds = Date.now(); // Get current UTC time in milliseconds
+const utcNowSeconds = Math.floor(utcNowMilliseconds / 1000); // UTC time in seconds
+
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const privatekey = fs.readFileSync('private_key.pem');
+const payload = {
+    iss: import.meta.env.ISS,
+    iat: utcNowSeconds,
+    exp: 86400
+}
+
+
+const token = jwt.sign(payload, privatekey,{algorithm: 'ES256', kid: import.meta.env.KID })
 
 button.addEventListener("change", async function () {
     // Call configure() to configure an instance of MusicKit on the Web.
     console.log("2");
       try {
         await MusicKit.configure({
-          developerToken: "",
+          developerToken: token,
           app: {
             name: "applemusic",
             build: "25.11.1",
