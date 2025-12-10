aea9ff76a3b35017c52a0ed23310d41a4705e639
phadeline <phadelinee@gmail.com>
2025-12-10 14:34:58 -0500

    Count variable to ensure MusicKit is only configur ed once on mousemove event.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,66 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import "./App.css";
+import axios from "axios";
+/* global MusicKit */
 function App() {
+const documentRef = useRef(document);
+var instance = null;
+
+  axios.get("http://localhost:8000/token",{
+  }).then((response) => {
+     if (response.status == 200) {
+      console.log("Token fetched successfully");
+      return response.data;
+    }else if (response.status != 200){ 
+      console.error("Failed to fetch token");
+    }
+  }).then(data => {
+    sessionStorage.setItem("devtoken", data.token);
+  }).
+  catch((error) => {
+    console.error(error);
+  });
+
+
+var count = 0;
+
+useEffect(() => {  
+async function handleMouseMove(event) {
+  event.preventDefault();
+  if(count < 1){
+  console.log("musickitloaded event fired");
+  try {
+   // Call configure() to configure an instance of MusicKit on the Web.
+    await MusicKit.configure({
+      developerToken: sessionStorage.getItem("devtoken"),
+      app: {
+        name: "irecommend",
+      },
+    });
+    console.log(`success ${sessionStorage.getItem("devtoken")}`);
+  } catch (err) {
+    console.log(err);
+    // Handle configuration error
+  };
+   count++;
+  // MusicKit instance is available
+instance = MusicKit.getInstance();}
+}
+
+
+document.addEventListener("mousemove", handleMouseMove);
+return() => {document.removeEventListener("mousemove", handleMouseMove)};
+
+},[])
+
   return (
     <div className="App" style={{ textAlign: "center" }}>
       <h1 style={{ textAlign: "center" }}>Welcome to i-Recommend</h1>
-      <div className="card" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <div className="titlebox" style={{ marginBottom: "20px" }}>
+      <div className="card" >
+        <div className="titlebox" >
           <h1 id="title">iRecommend</h1>
         </div>
-        <div className="connectbox" style={{ marginTop: "20px" }}>
+        <div className="connectbox" >
           <div>
             <input
               type="range"
@@ -17,6 +69,7 @@ function App() {
               defaultValue="10"
               className="slider"
               id="myRange"
+         
             />
           </div>
           <div className="textdiv">
