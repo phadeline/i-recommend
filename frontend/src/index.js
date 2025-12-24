6c9df84f6c422e4fdb099cf876215e5553061b2b
phadeline <phadelinee@gmail.com>
2025-12-24 10:53:40 -0500

    feaf

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import PlaylistsLists from "./Components/playlistpage";
 import AllMusic from "./Components/AllMusic";
-import "../public/index.html"
+
 
 import reportWebVitals from "./reportWebVitals";
 import {
