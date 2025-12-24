a26d038dc91238ab04baa9daad53bbf94068dab2
phadeline <phadelinee@gmail.com>
2025-12-24 10:44:55 -0500

    ddf

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import PlaylistsLists from "./Components/playlistpage";
-import AllMusic from "./Components/AllMusic"
+import AllMusic from "./Components/AllMusic";
+import "../public/index.html"
 
 import reportWebVitals from "./reportWebVitals";
 import {
