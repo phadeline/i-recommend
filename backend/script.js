50f012a51be3edeecc4f3fc3e2d059d3079e57dd
phadeline <phadelinee@gmail.com>
2025-12-24 12:27:41 -0500

    ded

--- backend/script.js
+++ backend/script.js
@@ -46,7 +46,7 @@ app.listen(port, () => {
 });
 
 app.get("/" , (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html",)); ;  
+  res.sendFile(path.join(__dirname, "../frontend/public", "index.html",)); ;  
 });
 
 app.options("/", (req, res) => {
