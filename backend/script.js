7bb28c2e84d77d1d62f02d1c37cc5bdd5e9788bc
phadeline <phadelinee@gmail.com>
2025-12-29 12:50:20 -0500

    changes

--- backend/script.js
+++ backend/script.js
@@ -54,7 +54,7 @@ app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
 
-app.get("i-recommend/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(frontendBuildPath, "index.html"));
 });
 
