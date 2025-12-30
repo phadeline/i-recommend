fea86f8f7aa2edc9147d194f575b412629032d9a
phadeline <phadelinee@gmail.com>
2025-12-30 15:59:28 -0500

    change

--- frontend/src/Components/playlistpage.js
+++ frontend/src/Components/playlistpage.js
@@ -17,7 +17,7 @@ const PlaylistsLists = () => {
       <ul className="AllPlaylists">
         {myPlaylists ? (
         
-        myPlaylists.data?.map((playlist) => (
+        myPlaylists.data.map((playlist) => (
           
             <Link
             
