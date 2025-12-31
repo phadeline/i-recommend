885ad1f369cb0513e79c59b08fb440092a5e09d2
phadeline <phadelinee@gmail.com>
2025-12-31 16:30:50 -0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -71,7 +71,7 @@ function App() {
         event.preventDefault();
         const instance = await MusicKit.getInstance();
         console.log("click");
-        if (sliderValue == 200) {
+        if (sliderValue === "200") {
           console.log("slider maxed");
 
           if (!instance.isAuthorized) {
