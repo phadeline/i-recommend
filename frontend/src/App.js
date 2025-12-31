9aaea35bfe42d7b37eed0720fc684ab4a419ea07
phadeline <phadelinee@gmail.com>
2025-12-30 21:11:37 -0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -193,13 +193,13 @@ const decodedToken = decodeURIComponent(userToken);
           </div>
           <div className="textdiv">
           
-              <Link to="/playlists"  state={myPlaylists}>
+              <a href="/playlists"  state={myPlaylists}>
                 <button style={{display: "none"}}
                   type=" button"
                   onClick={getPlaylists}
                   id="image"
                 ></button>
-              </Link>
+              </a>
        
           </div>
         </div>
