4d3bb40956b68fe47c85718ee43f134bb3d08559
phadeline <phadelinee@gmail.com>
2025-12-31 00:14:47 -0500

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import "./App.css";
 import axios from "axios";
 //import "../../frontend/"
 
-
-
 /* global MusicKit */
 function App() {
   const musicPlaylists = "https://api.music.apple.com/v1/me/library/playlists"; // Example MusicKit API endpoint
@@ -15,104 +13,70 @@ function App() {
   var count = 0;
   const [sliderValue, setSliderValue] = useState("15");
 
-
   let [searchParams, setSearchParams] = useSearchParams();
-     const userToken = searchParams.get("music-user-token");
-const decodedToken = decodeURIComponent(userToken);
-  
+  const userToken = searchParams.get("music-user-token");
+  const decodedToken = decodeURIComponent(userToken);
 
-//http://localhost:8000/token
+  //http://localhost:8000/token
   useEffect(() => {
-
-    async function getToken(){
-     
-    const response = await axios.get("https://i-recommend-289e22b5c5f5.herokuapp.com/token", (e) => {
-        e.preventDefault();
-      })
-      try{
+    async function getToken() {
+      const response = await axios.get(
+        "https://i-recommend-289e22b5c5f5.herokuapp.com/token",
+        (e) => {
+          e.preventDefault();
+        }
+      );
+      try {
         sessionStorage.removeItem("devtoken");
         sessionStorage.setItem("devtoken", response.data);
-        console.log(response.data)
-      }
-      catch(error) {
+        console.log(response.data);
+      } catch (error) {
         console.error("your Error: " + error);
-      };
-  }
+      }
+    }
     getToken();
-    ;}, [sessionStorage.getItem("devtoken")]);
-
-
+  }, [sessionStorage.getItem("devtoken")]);
 
   useEffect(() => {
-  
     async function handleMouseMove(event) {
       event.preventDefault();
       const myToken = sessionStorage.getItem("devtoken");
-      
-      
-        console.log("musickitloaded event fired");
-        try {
-          // Call configure() to configure an instance of MusicKit on the Web.
-          await MusicKit.configure({
-            developerToken: myToken,
-            app: {
-              name: "irecommend",
-            },
-          });
-          //console.log(`success ${sessionStorage.getItem("devtoken")}`);
-        } catch (err) {
-          console.log("not a success: "+ err);
-          // Handle configuration error
-        }
-        count++;
-        console.log("in handlesmousemove: " + count);
-       document.removeEventListener("mousemove", handleMouseMove);
-   
+
+      console.log("musickitloaded event fired");
+      try {
+        // Call configure() to configure an instance of MusicKit on the Web.
+        await MusicKit.configure({
+          developerToken: myToken,
+          app: {
+            name: "irecommend",
+          },
+        });
+        //console.log(`success ${sessionStorage.getItem("devtoken")}`);
+      } catch (err) {
+        console.log("not a success: " + err);
+        // Handle configuration error
+      }
+      count++;
+      console.log("in handlesmousemove: " + count);
+      document.removeEventListener("mousemove", handleMouseMove);
     }
-    
+
     document.addEventListener("mousemove", handleMouseMove);
-
-
-   
   }, []);
 
-
-
   async function Click(event) {
-    
     event.preventDefault();
     //this refers to the button element
-   
-      const instance = MusicKit.getInstance();
-      if (sliderValue == "200" && instance.authorize) {
-        instance.unauthorize();
-        console.log("slider maxed");
-        if (!instance.isAuthorized) {
-          console.log("not authorized");
-          setSliderValue("200");
-          rangeSlider.style.opacity = "0.2";
-          rangeSlider.value = "200";
-          await instance
-            .authorize()
-            .then(function (token) {
-               
-              setSearchParams("");
-              window.location.href +=
-                "?music-user-token=" + encodeURIComponent(token);
-              //const playlists = instance.api.music("v1/me/library/playlists");
-              
-              button.style.display = "flex";
-             
 
-            })
-            .catch(function (err) {
-              console.error(err);
-            });
-        }
-      } else if (sliderValue !== "200") {
-        alert("Please click to the end of slider before connecting.");
-      } else {
-        console.log("not authorized2");
+    const instance = MusicKit.getInstance();
+    if (sliderValue == "200" && instance.authorize) {
+      instance.unauthorize();
+      console.log("slider maxed");
+      if (!instance.isAuthorized) {
+        console.log("not authorized");
+        setSliderValue("200");
+        rangeSlider.style.opacity = "0.2";
+        rangeSlider.value = "200";
         await instance
           .authorize()
           .then(function (token) {
@@ -120,64 +84,75 @@ const decodedToken = decodeURIComponent(userToken);
             window.location.href +=
               "?music-user-token=" + encodeURIComponent(token);
             //const playlists = instance.api.music("v1/me/library/playlists");
-           
+
+            button.style.display = "flex";
           })
           .catch(function (err) {
             console.error(err);
           });
-        return setSliderValue("200");
       }
-  
-  count++;
-  console.log("in instance event: " + count);
-   
+    } else if (sliderValue !== "200") {
+      alert("Please click to the end of slider before connecting.");
+    } else {
+      console.log("not authorized2");
+      await instance
+        .authorize()
+        .then(function (token) {
+          setSearchParams("");
+          window.location.href +=
+            "?music-user-token=" + encodeURIComponent(token);
+          //const playlists = instance.api.music("v1/me/library/playlists");
+        })
+        .catch(function (err) {
+          console.error(err);
+        });
+      return setSliderValue("200");
+    }
+
+    count++;
+    console.log("in instance event: " + count);
   }
 
-
-  
- 
-
-const [MyPlay, setMyPlay] = useState([]);
+  const [MyPlay, setMyPlay] = useState([]);
   useEffect(() => {
-  async function getPlaylists() {
-    
-  
-    console.log(decodedToken);
-    const getToken = sessionStorage.getItem("devtoken");
-   
+    async function getPlaylists() {
+      console.log(decodedToken);
+      const getToken = sessionStorage.getItem("devtoken");
 
-    axios
-      .get(musicPlaylists, {
-        headers: {
-          "Authorization": `Bearer ${getToken}`,
-          "Music-User-Token": `${decodedToken}`,
-          "Content-Type": "application/json",
-        },
-      }).then((response)=>{
-       console.log(response);
+      axios
+        .get(musicPlaylists, {
+          headers: {
+            Authorization: `Bearer ${getToken}`,
+            "Music-User-Token": `${decodedToken}`,
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          console.log(response);
 
-       return response
-        // Process the playlist data here
-         
-       }).then((data)=>{
-  
-       setMyPlay(data.data);
-       documentRef.current.getElementById("image").removeEventListener("click", getPlaylists);
+          return response;
+          // Process the playlist data here
+        })
+        .then((data) => {
+          setMyPlay(data.data);
+          documentRef.current
+            .getElementById("image")
+            .removeEventListener("click", getPlaylists);
+        })
+        .catch((error) => {
+          console.error("Error fetching playlists:", error);
+        });
+    }
+    documentRef.current
+      .getElementById("image")
+      .addEventListener("click", getPlaylists);
+  }, []);
 
-       }).catch((error) =>{console.error("Error fetching playlists:", error)});
-
-      } documentRef.current.getElementById("image").addEventListener("click", getPlaylists);
-      
-      
-      
-    }, []);
-
-     
- 
-  
   return (
     <div className="App" style={{ textAlign: "center" }}>
-      <h1 className="welcome" style={{ textAlign: "center" }}>Welcome to i-Recommend</h1>
+      <h1 className="welcome" style={{ textAlign: "center" }}>
+        Welcome to i-Recommend
+      </h1>
       <div className="card">
         <div className="titlebox">
           <h1 id="title">iRecommend</h1>
@@ -199,22 +174,22 @@ const [MyPlay, setMyPlay] = useState([]);
             />
           </div>
           <div className="textdiv">
-          
-              <Link to={"/playlists"} state={{MyPlay: MyPlay}}>
-                <button style={{display: "none"}}
-                  type=" button"
-                  id="image"
-                ></button>
-              </Link>
-       
+            <Link to={"/playlists"} state={{ MyPlay: MyPlay }}>
+              <button
+                style={{ display: "none" }}
+                type=" button"
+                id="image"
+              ></button>
+            </Link>
           </div>
         </div>
-        <div ><p>
-          DoubleClick the end of the slider to connect your Apple Music account. 
-        </p>
-        <p>Then press Connect.</p>
+        <div>
+          <p>
+            DoubleClick the end of the slider to connect your Apple Music
+            account.
+          </p>
+          <p>Then press Connect.</p>
         </div>
-        
       </div>
     </div>
   );
