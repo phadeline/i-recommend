7cf2d77365d5760a77ce59be37434d025d6df22f
phadeline <phadelinee@gmail.com>
2026-02-24 07:08:14 -0500

    changes

--- frontend/src/Components/recommendations.js
+++ frontend/src/Components/recommendations.js
@@ -18,7 +18,7 @@ const [genres, setGenres] = useState([]);
    
     const FetchAllGenres = async () => {
       try {
-        let searchTerm = genreName[2] ? genreName[2] || genreName[1] : genreName[0];
+        let searchTerm = genreName[1] ? genreName[1] : genreName[0];
         console.log("Search term for recommendations: " + searchTerm);
         const response = await axios.get(`https://api.music.apple.com/v1/catalog/us/search?types=songs&limit=10&term=${searchTerm}`, {
           headers: {
