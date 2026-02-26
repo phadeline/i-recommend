68927a9d819a05f645bce5aa9bebfaa6cc01e169
phadeline <phadelinee@gmail.com>
2026-02-25 22:23:36 -0500

    phay

--- frontend/src/Components/recommendations.js
+++ frontend/src/Components/recommendations.js
@@ -94,7 +94,7 @@ console.log(randomIndex);
       <h3 style={{ marginLeft: "20px" }}>Because you like:</h3>
       <h4>{songName}</h4>
       {genres ? (
-        genres.data?.[randomIndex]?.map((song) => (
+        genres.data?.map(randomIndex)?.map((song) => (
           <p
             className="recommendedArtist"
             key={song.attributes.id}
