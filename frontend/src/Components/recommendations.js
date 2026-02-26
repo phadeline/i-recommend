8c0854e55c3ea9296cd97c786a28bbfea25ac232
phadeline <phadelinee@gmail.com>
2026-02-25 21:28:45 -0500

    changes

--- frontend/src/Components/recommendations.js
+++ frontend/src/Components/recommendations.js
@@ -24,9 +24,9 @@ function Recommendations({ genreName, Token, artistName, songName }) {
             },
           },
         );
-        for (let i = 0; i < response.data.results.songs.length; i++) {
+        for (let i = 0; i < response.data.length; i++) {
           console.log("Number of results found for artist: ")
-          if (response.data.results.songs[i] = response.status===400) {
+          if (response.data.results.songs[i]  = response.status ===400) {
             console.log("No results found for artist: " + artistName + ". Trying song name: " + songName);
             const response2 = await axios.get(
               `https://api.music.apple.com/v1/catalog/us/search?types=songs&limit=10&term=${searchTerm}`,
