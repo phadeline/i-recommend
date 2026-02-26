59920d7705484f9c7a6a960f0917b5ecaa401776
phadeline <phadelinee@gmail.com>
2026-02-25 23:02:54 -0500

    changes

--- frontend/src/Components/recommendations.js
+++ frontend/src/Components/recommendations.js
@@ -94,14 +94,14 @@ console.log(randomIndex);
       <h3 style={{ marginLeft: "20px" }}>Because you like:</h3>
       <h4>{songName}</h4>
       {genres ? (
-         randomIndex.map((index) => genres.data[index]).map((song) => (
+         randomIndex.map((index) => genres.data[index] && (
           <p
             className="recommendedArtist"
-            key={song.attributes.id}
-            onMouseEnter={() => playSongPreview(song.attributes.playParams.id)}
+            key={genres.data[index].attributes.id}
+            onMouseEnter={() => playSongPreview(genres.data[index].attributes.playParams.id)}
             style={{ margin: "10px" }}
           >
-            {song.attributes.artistName} - {song.attributes.name}
+            {genres.data[index].attributes.artistName} - {genres.data[index].attributes.name}
             <div>
               <button
                 style={{
